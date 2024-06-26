c592139cb82038baae91d93810b4a02b6749dd59
Remntar36 <5721100454@utrng.edu.mx>
2024-06-26 11:34:50 -0600

    Agreganco mensaje para el try catch

--- Myron/script_menu.js
+++ Myron/script_menu.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 moviesTable.style.display = 'table';
                 hideMoviesButton.style.display = 'inline-block';
             })
-            .catch(error => console.error('Error fetching data:', error));
+            .catch(error => console.error('Error data:', error));
     });
 
     hideMoviesButton.addEventListener('click', () => {
